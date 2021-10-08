dbf330a72c0d3ae00d85617c9e0fec15e8426fb9
Tawana Kwaramba <tawanakwaramba@192-168-1-102.tpgi.com.au>
2021-10-09 00:07:22 +0800

    all layers for viewing students have being added

--- app/src/main/java/com/example/asstwo/StudentViewing.java
+++ app/src/main/java/com/example/asstwo/StudentViewing.java
@@ -1,13 +1,41 @@
 package com.example.asstwo;
 
+import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class StudentViewing extends AppCompatActivity {
 
+    private Graph mathTestGraph;
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(StudentViewing.this, MainActivity.class);
+        startActivity(intent);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_viewing);
+
+        //loading the graph when this is created
+        mathTestGraph = new Graph();
+        mathTestGraph = mathTestGraph.load(StudentViewing.this);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mathTestGraph.save(StudentViewing.this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mathTestGraph.save(StudentViewing.this);
     }
 }
