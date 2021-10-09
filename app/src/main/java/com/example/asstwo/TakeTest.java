42df84415f4c80c210824d78d8d4ca2f9633bb71
Tawana Kwaramba <tawanakwaramba@192-168-1-102.tpgi.com.au>
2021-10-10 03:30:06 +0800

    can get JSON text from teh server

--- app/src/main/java/com/example/asstwo/TakeTest.java
+++ app/src/main/java/com/example/asstwo/TakeTest.java
@@ -1,14 +1,28 @@
 package com.example.asstwo;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.AsynchronousChannelGroup;
+import java.security.GeneralSecurityException;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class TakeTest extends AppCompatActivity {
 
@@ -28,6 +42,9 @@ public class TakeTest extends AppCompatActivity {
     private QuestionButtons answerButtons;
     private AnswerInput inputAnswer;
 
+    private URL url;
+    private HttpsURLConnection conn;
+
 
 
     @Override
@@ -42,7 +59,7 @@ public class TakeTest extends AppCompatActivity {
         {
             banner.setText("Test: " + name);
             setUpButtonFragment();
-
+            new  MyTask().execute();
 
         }
         else
@@ -82,5 +99,99 @@ public class TakeTest extends AppCompatActivity {
         end = findViewById(R.id.endTest);
         next = findViewById(R.id.nextAnswers);
         prev = findViewById(R.id.previousAnswers);
+    }
+
+
+    private class MyTask extends AsyncTask<Void, Integer, String>
+    {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            connectToServer();
+            String result = null;
+            InputStream input = null;
+            ByteArrayOutputStream boas = null;
+
+            try
+            {
+                input = conn.getInputStream();
+                boas = new ByteArrayOutputStream();
+                //TODO: come back and add teh code for the progress bar
+
+                byte[] buffer = new byte[1024];
+                int bytesRead = input.read(buffer);
+                int progress = 0;
+
+                while (bytesRead > 0)
+                {
+                    boas.write(buffer, 0, bytesRead);
+                    bytesRead = input.read(buffer);
+                    progress += bytesRead;
+                    //TODO: add code for progress ba
+                }
+
+                boas.close();
+                result = new String(boas.toByteArray());
+            } catch (IOException e) {
+                Log.e(TAG, e.getMessage());
+                e.printStackTrace();
+            }
+            finally {
+                conn.disconnect();
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            //super.onPostExecute(s);
+
+            //getting  the json and displaying it on the screen
+            question.setText(s);
+
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
+
+        private void connectToServer()
+        {
+            //connecting to the server which we have just made for our application
+            String urlString =
+                    Uri.parse("https://192.168.1.102:8000/random/question/")
+                    .buildUpon()
+                    .appendQueryParameter("method", "thedata.getit")
+                    .appendQueryParameter("api_key", "01189998819991197253")
+                    .appendQueryParameter("format", "json")
+                    .build().toString();
+
+            Log.e(TAG, "found url: " + urlString);
+
+            try
+            {
+                url = new URL(urlString);
+                conn = (HttpsURLConnection) url.openConnection();
+                // telling androi studio to trust the certificate which we just create for ourselves
+                DownloadUtils.addCertificate(TakeTest.this, conn);
+
+                if(conn.getResponseCode() != HttpURLConnection.HTTP_OK)
+                {
+                    Log.e(TAG, "Can't not access requrested website");
+                }
+            } catch (GeneralSecurityException e) {
+                Log.e(TAG, e.getMessage());
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                Log.e(TAG, e.getMessage());
+                e.printStackTrace();
+            } catch (IOException e) {
+                Log.e(TAG, e.getMessage());
+                e.printStackTrace();
+            }
+
+        }
     }
 }
